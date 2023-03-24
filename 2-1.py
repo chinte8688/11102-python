620205a5e721ff4d92cafd2fa48d6d2c6c809e16
chinte8688 <22674461+chinte8688@users.noreply.github.com>
2023-03-24 15:44:04 +0800

    Add files via upload

---
+++ 2-1.py
@@ -0,0 +1,7 @@
+# 1年可存多少錢
+hourly_salary = 125     # 時薪
+annual_salary = hourly_salary * 8 * 300 # 年薪
+monthly_fee = 9000      # 月支出    
+annual_fee = monthly_fee *12    
+annual_savings = annual_salary - annual_fee # 年度存款
+print(annual_savings)
