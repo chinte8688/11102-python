a0895ac1d53bfab41cb9134bea7ae0091d4b0dbf
chinte8688 <22674461+chinte8688@users.noreply.github.com>
2023-03-24 11:59:47 +0800

    Add files via upload

---
+++ 2-7.py
@@ -0,0 +1,12 @@
+# 計算周長與圓面積
+# 周長 = 2*PI*r
+# 園面積 = PI*r*r
+# PI = 3.14159  r = 半徑
+PI = 3.14159
+r = 15
+circum_ference = 2 * PI *r
+area = PI * r * r
+print("本題周長:")
+print(circum_ference)
+print("本題園面積:")
+print(area)
