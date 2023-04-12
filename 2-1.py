5f0659f3d4cddf8b34be6932e9ebd88f9a5dfc44
chinte8688 <22674461+chinte8688@users.noreply.github.com>
2023-04-12 09:28:24 +0800

    20330412改

--- 2-1.py
+++ 2-1.py
@@ -6,5 +6,5 @@ annual_fee = monthly_fee *12
 annual_savings = annual_salary - annual_fee # 年度存款
 print(annual_savings)
 print("年度收入 %d 年度支出 %d 年度結餘 %d" % (annual_salary,annual_fee,annual_savings))
-print("年度收入 {0:%d} 年度支出 {1:%d} 年度結餘 {2:%d}").format(annual_salary,annual_fee,annual_savings)
+print("年度收入 {0:d} 年度支出 {1:d} 年度結餘 {2:d}".format(annual_salary,annual_fee,annual_savings))
 print("朱晉德")
