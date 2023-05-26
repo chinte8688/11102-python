edfb6714425e866ed89f10fa57f7084a3d6fa02c
chinte8688 <22674461+chinte8688@users.noreply.github.com>
2023-05-26 23:06:43 +0800

    052601

--- ex.py
+++ ex.py
@@ -1,3 +1,5 @@
+numbers = input("請輸入六個整數（以空格分隔）：")
+numbers = numbers.split()  # 將輸入以空格分隔成一個數字列表
 
 a,b,c,d = eval(input("請輸入 4 個數字以,號分開："))
 # d = int(a) + int(b) + int(c)
@@ -34,3 +36,16 @@ while True :
     #    print("第 %d 圈 %d %d %d %d 排序完畢02" % (i,a,b,c,d))
         #continue
         #break
+
+
+# 使用迴圈和判斷式進行排序
+for i in range(len(numbers)):
+    print("i 第 %d 圈" % (i), end=(' '))
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] > numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+        print("j 第 %d 圈" % (j), end=(' '))    
+        print(numbers)  # 印出每一次交換後的排序結果
+
+# 輸出最終排序結果
+print("最終排序結果：", numbers)
