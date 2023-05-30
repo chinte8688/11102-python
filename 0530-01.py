e43c7ca90e17bf9b547f6c01667b3bf7dc398c13
chinte8688 <22674461+chinte8688@users.noreply.github.com>
2023-05-30 08:40:32 +0800

    ５數 0530-01

---
+++ 0530-01.py
@@ -0,0 +1,53 @@
+a,b,c,d,e = eval(input("請輸入 5 個數字以[,]號分開："))
+
+print("您輸入的數字為 %d %d %d %d %d" % (a,b,c,d,e))
+i = 0                                             # 圈數  
+j = 0                                             # 交換數  
+swap = 0                                          # 交換空間
+
+if a < b and b < c and c < d and d < e:           # if 由小排到大 = True
+        print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢01" % (i,j,a,b,c,d,e))
+        
+while a > b or b > c or c > d or d > e:             # 排序未完成 
+    i += 1                                          # 圈數計算
+    if a > b or b > c or c > d or d > e: 
+        if d > e :     
+            swap = d
+            d = e
+            e = swap
+            j += 1
+              
+            if a < b and b < c and c < d and d < e:
+                print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢02" % (i,j,a,b,c,d,e))
+                break
+            else:
+                print("第%02d圈%02d次交換 %d %d %d %d %d" % (i,j,a,b,c,d,e))
+    else:
+        continue
+
+    if a > b or b > c or c > d: 
+           if c > d :     
+              swap = c
+              c = d
+              d = swap
+              j += 1
+              print("第%02d圈%02d次交換 %d %d %d %d %d" % (i,j,a,b,c,d,e))
+    else:
+            continue
+    if a > b or b > c:      
+            if b > c:
+               swap = b
+               b = c
+               c = swap
+               j += 1
+               print("第%02d圈%02d次交換 %d %d %d %d %d" % (i,j,a,b,c,d,e))
+    else:    
+            continue
+    if a > b :          
+            swap = a
+            a = b
+            b = swap
+            j += 1
+            print("第%02d圈%02d次交換 %d %d %d %d %d" % (i,j,a,b,c,d,e))
+    else:
+            continue 
