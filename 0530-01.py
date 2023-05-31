170daa8be1a183cbf32d1263527890b83bf68cc9
chinte8688 <22674461+chinte8688@users.noreply.github.com>
2023-05-31 17:08:51 +0800

    0531-01

--- 0530-01.py
+++ 0530-01.py
@@ -1,23 +1,23 @@
 n1,n2,n3,n4,n5 = eval(input("請輸入 5 個數字以[,]號分開："))
 
 print("您輸入的數字為 %d %d %d %d %d" % (n1,n2,n3,n4,n5))
-i = 0                                             # 圈數  
-j = 0                                             # 交換數  
-swn1p = 0                                          # 交換空間
+i = 0                                                # 圈數  
+j = 0                                                # 交換數  
+swn1p = 0                                            # 交換空間
 
-if n1 < n2 and n2 < n3 and n3 < n4 and n4 < n5:           # if 由小排到大 = Trun5
-        print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢01" % (i,j,n1,n2,n3,n4,n5))
-        
-while n1 > n2 or n2 > n3 or n3 > n4 or n4 > n5:             # 排序未完成 
-    i += 1                                          # 圈數計算
-    if n1 > n2 or n2 > n3 or n3 > n4 or n4 > n5: 
-        if n4 > n5 :     
-            swn1p = n4
-            n4 = n5
-            n5 = swn1p
-            j += 1
+if n1 <= n2 and n2 <= n3 and n3 <= n4 and n4 <= n5:  # if 一開始就由小排到大
+        print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢01" % (i,j,n1,n2,n3,n4,n5)) #排序完成
+
+while n1 >= n2 or n2 >= n3 or n3 >= n4 or n4 >= n5:  # 排序未完成，進入迴圈 
+    i += 1                                           # 圈數計算
+    if n1 >= n2 or n2 >= n3 or n3 >= n4 or n4 >= n5: # 如果有前數 > 後數的狀況
+        if n4 >= n5 :                                # 如果 n4 >= n5    
+            swn1p = n4                               #  
+            n4 = n5                                  #   n4,n5 對調
+            n5 = swn1p                               #   
+            j += 1                                   # 交換次數 +1   
               
-            if n1 < n2 and n2 < n3 and n3 < n4 and n4 < n5:
+            if n1 <= n2 and n2 <= n3 and n3 <= n4 and n4 <= n5:
                 print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢02" % (i,j,n1,n2,n3,n4,n5))
                 break
             else:
@@ -25,14 +25,14 @@ while n1 > n2 or n2 > n3 or n3 > n4 or n4 > n5:             # 排序未完成
     else:
         continue
 
-    if n1 > n2 or n2 > n3 or n3 > n4: 
-        if n3 > n4 :     
+    if n1 >= n2 or n2 >= n3 or n3 >= n4: 
+        if n3 >= n4 :     
             swn1p = n3
             n3 = n4
             n4 = swn1p
             j += 1
 
-            if n1 < n2 and n2 < n3 and n3 < n4 and n4 < n5:
+            if n1 <= n2 and n2 <= n3 and n3 <= n4 and n4 <= n5:
                 print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢03" % (i,j,n1,n2,n3,n4,n5))
                 break
             else:
@@ -41,14 +41,14 @@ while n1 > n2 or n2 > n3 or n3 > n4 or n4 > n5:             # 排序未完成
     else:
             continue
     
-    if n1 > n2 or n2 > n3:      
-        if n2 > n3:
+    if n1 >= n2 or n2 >= n3:      
+        if n2 >= n3:
            swn1p = n2
            n2 = n3
            n3 = swn1p
            j += 1
 
-           if n1 < n2 and n2 < n3 and n3 < n4 and n4 < n5:
+           if n1 <= n2 and n2 <= n3 and n3 <= n4 and n4 <= n5:
                 print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢04" % (i,j,n1,n2,n3,n4,n5))
                 break
            else:
@@ -57,13 +57,13 @@ while n1 > n2 or n2 > n3 or n3 > n4 or n4 > n5:             # 排序未完成
     else:    
             continue
     
-    if n1 > n2 :          
+    if n1 >= n2 :          
             swn1p = n1
             n1 = n2
             n2 = swn1p
             j += 1
 
-            if n1 < n2 and n2 < n3 and n3 < n4 and n4 < n5:
+            if n1 <= n2 and n2 <= n3 and n3 <= n4 and n4 <= n5:
                 print("第%02d圈%02d次交換 %d %d %d %d %d 排序完畢05" % (i,j,n1,n2,n3,n4,n5))
                 break
             else:
